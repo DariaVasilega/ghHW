dcb0e1f5ce8f0bcf064f144102daf34871896e93
DariaVasilega <dariavasilega@gmail.com>
2019-11-15 21:42:49 +0200

    with mail service

--- src/Service/Notify.php
+++ src/Service/Notify.php
@@ -4,7 +4,35 @@
 namespace App\Service;
 
 
+use Swift_Mailer;
+
 class Notify
 {
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+    /**
+     * @var string
+     */
+    private $sendFrom;
 
+    public function __construct(Swift_Mailer $mailer, string $sendFrom)
+    {
+        $this->mailer = $mailer;
+        $this->sendFrom = $sendFrom;
+    }
+
+    public function notify(string $sendTo)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom($this->sendFrom)
+            ->setTo($sendTo)
+            ->setBody(
+                "<html><body>Hello</body></html>",
+                'text/html'
+            )
+        ;
+        $this->mailer->send($message);
+    }
 }
